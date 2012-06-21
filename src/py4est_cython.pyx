f03da90c7e749b2e9ec182b2c5240638420b7ea5
Adrian Seyboldt <adrian_seyboldt@web.de>
2012-06-21 23:42:30 +0200

    add extension types for ghost, mesh and Forrest + leaf iterator

--- src/py4est_cython.pyx
+++ src/py4est_cython.pyx
@@ -6,10 +6,14 @@ from mpi4py import MPI
 _LP = 0
 
 from _c_p4est_base cimport p4est_init, p4est_topidx_t
-from _c_p4est cimport p4est_t, p4est_quadrant_t, p4est_init_t, p4est_destroy
+from _c_p4est cimport (p4est_t, p4est_quadrant_t, p4est_init_t,
+                       p4est_destroy, p4est_tree_t, p4est_tree_array_index)
 from _c_p4est_connectivity cimport (p4est_connectivity_t,
                                     p4est_connectivity_new_unitsquare,
-                                    p4est_connectivity_destroy)
+                                    p4est_connectivity_destroy,
+                                    P4EST_CONNECT_FULL)
+from _c_p4est_ghost cimport p4est_ghost_t, p4est_ghost_new, p4est_ghost_destroy
+from _c_p4est_mesh cimport p4est_mesh_t, p4est_mesh_new, p4est_mesh_destroy
 from _c_p4est_extended cimport p4est_new_ext
 from _c_sc cimport sc_init, sc_finalize
 
@@ -145,8 +149,88 @@ cdef class P4est(object):
         self._p4est = NULL
         self._con = None 
 
-    def quadrants(self):
-        pass
+
+cdef class Ghost:
+    cdef p4est_ghost_t *_ghost
+    cdef P4est _p4est
+
+    def __init__(self, P4est p4est not None):
+        self._ghost = p4est_ghost_new(p4est._p4est, P4EST_CONNECT_FULL)
+        self._p4est = p4est
+
+    def __dealloc__(self):
+        if self._ghost is not NULL:
+            p4est_ghost_destroy(self._ghost)
+
+cdef class Mesh:
+    cdef p4est_mesh_t *_mesh
+    cdef Ghost _ghost
+    cdef P4est _p4est
+
+    def __init__(self, P4est p4est not None, Ghost ghost not None):
+        self._mesh = p4est_mesh_new(p4est._p4est, ghost._ghost,
+                                    P4EST_CONNECT_FULL)
+        self._p4est = p4est
+        self._ghost = ghost
+
+    def __dealloc__(self):
+        if self._mesh is not NULL:
+            p4est_mesh_destroy(self._mesh)
+
+
+cdef class Leaf:
+    cdef p4est_topidx_t _which_tree
+    cdef p4est_topidx_t _which_quad
+    cdef p4est_tree_t *_tree
+
+    cdef Forrest _forrest
+
+    def __init__(self, Forrest forrest not None):
+        self._forrest = forrest
+
+    def __str__(self):
+        return "Leaf(which_tree={}, which_quad={})".format(
+                  self._which_tree, self._which_quad)
+
+
+cdef class Forrest:
+    cdef P4est _p4est
+    cdef Connectivity _con
+    cdef Ghost _ghost
+    cdef Mesh _mesh
+
+    def __init__(self, connectivity, init_callback):  # TODO arguments?
+        self._con = Connectivity(connectivity)
+        self._p4est = P4est(self._con, user_data_init_callback=init_callback)
+        self._ghost = Ghost(self._p4est)
+        self._mesh = Mesh(self._p4est, self._ghost)
+
+    def leafs(self):
+        cdef p4est_t *p4est = self._p4est._p4est
+        
+        if p4est.local_num_quadrants == 0:
+            raise StopIteration
+
+        cdef Leaf leaf = Leaf(self)
+        leaf._which_tree = p4est.first_local_tree
+        leaf._tree = p4est_tree_array_index(p4est.trees, leaf._which_tree)
+        leaf._which_quad = 0
+
+        while True:
+            yield leaf
+            
+            if <size_t> leaf._which_quad + 1 == leaf._tree.quadrants.elem_count:
+                leaf._which_tree += 1
+                if leaf._which_tree  > p4est.last_local_tree:
+                    raise StopIteration
+                
+                leaf._tree = p4est_tree_array_index(p4est.trees, leaf._which_tree)
+                leaf._which_quad = 0;
+            else:
+                leaf._which_quad += 1
+
+
+
 
 
 #############################################
